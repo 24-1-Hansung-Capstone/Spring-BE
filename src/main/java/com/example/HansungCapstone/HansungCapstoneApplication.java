0454c5efa8c43cd2deda14f6cad550bc7c078436
jin3811 <hjwoo1028@naver.com>
2024-02-07 14:02:14 +0900

    first init

---
+++ src/main/java/com/example/HansungCapstone/HansungCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.HansungCapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HansungCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HansungCapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HansungCapstone/HansungCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HansungCapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HansungCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
