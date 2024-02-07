953de835afa2cc9ce87a5a9b3f6b84da6eca0e50
jin3811 <hjwoo1028@naver.com>
2024-02-07 17:51:20 +0900

    스프링 controller api 테스트

---
+++ src/main/java/com/example/HansungCapstone/TestController.java
@@ -0,0 +1,18 @@
+package com.example.HansungCapstone;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/testapi")
+public class TestController {
+
+    @GetMapping("/gettest")
+    public String getTest() {
+        return "Get Well";
+    }
+
+    @PostMapping("/posttest")
+    public String postTest() {
+        return "Post Well";
+    }
+}
