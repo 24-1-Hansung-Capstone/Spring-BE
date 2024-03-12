065f9ca6045605da1c31228cb1424243221263bf
lee <sjlee9908@naver.com>
2024-03-12 20:31:46 +0900

    Update SearchController.java

--- src/main/java/com/example/HansungCapstone/SearchController.java
+++ src/main/java/com/example/HansungCapstone/SearchController.java
@@ -2,14 +2,35 @@ package com.example.HansungCapstone;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000",}) // TODO : cors 설정 필요할 수 있음
 public class SearchController {
 
     @GetMapping("/search")
-    public String search(@RequestParam("query") String query) {
+    public ArrayList<SearchResult> search(@RequestParam("query") String query) {
         // TODO : 반환은 json으로 하도록 변경해야함
 
-        return "검색어 " + query + "에 대한 검색결과입니다.";
+        ArrayList<SearchResult> searchResults = new ArrayList<>();
+        for(int i = 0; i<500; i++){
+            searchResults.add(new SearchResult(query+i, query+i, query+i, query+i));
+        }
+
+        return searchResults;
+    }
+
+    static class SearchResult{
+        public String url;
+        public String title;
+        public String category;
+        public String preview;
+
+        public SearchResult(String url, String title, String category, String preview) {
+            this.url = url;
+            this.title = title;
+            this.category = category;
+            this.preview = preview;
+        }
     }
 }
