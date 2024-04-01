fdc627616232fbda9bad5495f546a564ddab631b
lee <sjlee9908@naver.com>
2024-04-01 20:59:59 +0900

    대구석 레포 수정

--- src/main/java/com/example/HansungCapstone/Repository/EsVisitkoreaRepository.java
+++ src/main/java/com/example/HansungCapstone/Repository/EsVisitkoreaRepository.java
@@ -1,6 +1,8 @@
 package com.example.HansungCapstone.Repository;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.query_dsl.MatchQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import com.example.HansungCapstone.DTO.Es.EsDto;
 import com.example.HansungCapstone.DTO.Es.Impl.EsVisitkoreaDto;
@@ -20,12 +22,34 @@ public class EsVisitkoreaRepository{
     public List<EsDto> search(String query) throws IOException {
         ArrayList<EsDto> results = new ArrayList<>();
 
+        Query byLocation = MatchQuery.of(m-> m
+                .field("location")
+                .query(query)
+        )._toQuery();
+
+        Query byTitle = MatchQuery.of(m-> m
+                .field("title")
+                .query(query)
+        )._toQuery();
+
+        Query byDescription = MatchQuery.of(m-> m
+                .field("description")
+                .query(query)
+        )._toQuery();
+
+        Query byTags = MatchQuery.of(m-> m
+                .field("tags")
+                .query(query)
+        )._toQuery();
+
         SearchResponse<EsVisitkoreaDto> search = elasticsearchClient.search(s -> s
                         .index("visitkorea")
                         .query(q -> q
-                                .term(t -> t
-                                        .field("location")
-                                        .value(v -> v.stringValue(query))
+                                .bool(b -> b
+                                        .should(byLocation)
+                                        .should(byTitle)
+                                        .should(byDescription)
+                                        .should(byTags)
                                 )),
                 EsVisitkoreaDto.class);
 
