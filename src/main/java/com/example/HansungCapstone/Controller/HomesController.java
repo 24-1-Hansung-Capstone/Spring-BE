3f1526e43ecbe00cd7e12a3e3b83c4fcd27a5538
kyungwonBang <ruddnjs8764@naver.com>
2024-05-03 17:47:33 +0900

    type 타입 변경
    타입 변경

--- src/main/java/com/example/HansungCapstone/Controller/HomesController.java
+++ src/main/java/com/example/HansungCapstone/Controller/HomesController.java
@@ -1,6 +1,7 @@
 package com.example.HansungCapstone.Controller;
 
 import com.example.HansungCapstone.DTO.Homes.Realty;
+import com.example.HansungCapstone.DTO.Homes.RealtyDto;
 import com.example.HansungCapstone.DTO.Homes.Type;
 import com.example.HansungCapstone.Service.Homes.HomesService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,28 +10,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/CommunityPage")
 @CrossOrigin(origins = {"http://localhost:3000",}) // TODO : cors 설정 필요할 수 있음
 public class HomesController {
 
     @Autowired
     private HomesService homesService;
 
-//    @PostMapping("/CommunityPage")
-//    public void register(@RequestBody RealtyDto realtyDto) {
-//        homesService.register(realtyDto);
-//    }
+    @PostMapping("/register")
+    public void register(@RequestBody RealtyDto realtyDto) {
+        homesService.register(realtyDto);
+    }
 
-    @GetMapping("/CommunityPage")
+    @GetMapping("/")
     public List<Realty> findAll() {
         return homesService.findAll();
     }
 
-    @GetMapping("/CommunityPage/name")
+    @GetMapping("/name")
     public List<Realty> findByName(@RequestParam String name) {
         return homesService.findByName(name);
     }
 
-    @GetMapping("/CommunityPage/type")
+    @GetMapping("/type")
     public List<Realty> findByType(@RequestParam Type type) {
         return homesService.findByType(type);
     }
--- src/main/java/com/example/HansungCapstone/DTO/Homes/Type.java
+++ src/main/java/com/example/HansungCapstone/DTO/Homes/Type.java
@@ -1,5 +1,15 @@
 package com.example.HansungCapstone.DTO.Homes;
 
 public enum Type {
-    JEONSE,MONTH,SALE,CONFER;
+    JEONSE(1), MONTH(2), SALE(3), CONFER(4);
+
+    private final int value;
+
+    Type(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
