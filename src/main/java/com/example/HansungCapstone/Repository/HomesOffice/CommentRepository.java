fd2af3629e62142ed2eb8484ca6ceb1752510b27
kyungwonBang <ruddnjs8764@naver.com>
2024-05-24 15:44:21 +0900

    ㅇㅇ
    ㅇㅇ

--- src/main/java/com/example/HansungCapstone/Repository/HomesOffice/CommentRepository.java
+++ src/main/java/com/example/HansungCapstone/Repository/HomesOffice/CommentRepository.java
@@ -2,10 +2,12 @@ package com.example.HansungCapstone.Repository.HomesOffice;
 
 import com.example.HansungCapstone.DTO.HomesOffice.RealtyComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CommentRepository extends JpaRepository<RealtyComment, Long> {
     List<RealtyComment> findAllByRealty(int RealtyId);
     Optional<RealtyComment> findByCommentId(int CommentId);
