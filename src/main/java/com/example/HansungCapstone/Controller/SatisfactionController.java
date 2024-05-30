4f15fa11375e7611dc129087d1fc240e8f6f4944
jin3811 <hjwoo1028@naver.com>
2024-05-30 19:51:58 +0900

    매물만족도 검색어 오탈자 수정 기능 추가

--- src/main/java/com/example/HansungCapstone/Controller/SatisfactionController.java
+++ src/main/java/com/example/HansungCapstone/Controller/SatisfactionController.java
@@ -1,14 +1,21 @@
 package com.example.HansungCapstone.Controller;
 
 import com.example.HansungCapstone.DTO.Es.EsDto;
+import com.example.HansungCapstone.DTO.Es.EsDtoWrapper;
 import com.example.HansungCapstone.Service.Es.EsSatisfactionService;
+import com.example.HansungCapstone.Service.Es.TypoCurrectionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RestController
@@ -20,8 +27,28 @@ public class SatisfactionController {
     @Autowired
     private EsSatisfactionService esSatisfactionService;
 
+    @Autowired
+    private TypoCurrectionService typoCurrectionService;
+
     @GetMapping("/satisfaction")
-    public List<EsDto> search(@RequestParam String query) throws IOException {
-        return esSatisfactionService.search(query);
+    public ResponseEntity<List<EsDto>> search(@RequestParam String query) throws IOException {
+        boolean isQueryChanged = false;
+
+        // 오탈차 전처리
+        String newQuery = typoCurrectionService.correctTypo(query);
+
+        // 오탈자 수정으로 인해 변경이 발생하면, 해당 사항을 프론트에 전달
+        isQueryChanged = !newQuery.isEmpty();
+
+        // 검색
+        List<EsDto> searchResult = esSatisfactionService.search(query);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("isQueryChanged", String.valueOf(isQueryChanged));
+        if (isQueryChanged) {
+            headers.add("suggestQuery", URLEncoder.encode(newQuery, StandardCharsets.UTF_8));
+        }
+
+        return new ResponseEntity<>(searchResult, headers, HttpStatus.OK);
     }
 }
