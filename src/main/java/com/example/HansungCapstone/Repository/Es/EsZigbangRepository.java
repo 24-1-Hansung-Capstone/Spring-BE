e5a4e8c5dfad06540f7d32d5189728108f63a4ee
lee <sjlee9908@naver.com>
2024-05-30 16:19:55 +0900

    Update EsZigbangRepository.java

--- src/main/java/com/example/HansungCapstone/Repository/Es/EsZigbangRepository.java
+++ src/main/java/com/example/HansungCapstone/Repository/Es/EsZigbangRepository.java
@@ -3,8 +3,10 @@ package com.example.HansungCapstone.Repository.Es;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._types.query_dsl.MatchQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch._types.query_dsl.TextQueryType;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import com.example.HansungCapstone.DTO.Es.EsDto;
+import com.example.HansungCapstone.DTO.Es.Impl.EsBlogDto;
 import com.example.HansungCapstone.DTO.Es.Impl.EsVisitkoreaDto;
 import com.example.HansungCapstone.DTO.Es.Impl.EsZigbangDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,18 +55,31 @@ public class EsZigbangRepository {
                 .query(query)
         )._toQuery();
 
+//        SearchResponse<EsZigbangDto> search = elasticsearchClient.search(s -> s
+//                        .index("zigbang")
+//                        .query(q -> q
+//                                .bool(b -> b
+//                                        .should(byDanjiName)
+//                                        .should(byTotalDesc)
+//                                        .should(byTrafficDesc)
+//                                        .should(byCareDesc)
+//                                        .should(byResidentDesc)
+//                                        .should(byAroundDesc)
+//                                )),
+//                EsZigbangDto.class);
+
         SearchResponse<EsZigbangDto> search = elasticsearchClient.search(s -> s
                         .index("zigbang")
-                        .query(q -> q
-                                .bool(b -> b
-                                        .should(byDanjiName)
-                                        .should(byTotalDesc)
-                                        .should(byTrafficDesc)
-                                        .should(byCareDesc)
-                                        .should(byResidentDesc)
-                                        .should(byAroundDesc)
-                                )),
-                EsZigbangDto.class);
+                        .size(100000)
+                        .query(q->q
+                                .multiMatch(v -> v
+                                        .fields("danji_name^2", "title^2")
+                                        .type(TextQueryType.MostFields)
+                                        .query(query)
+                                )
+                        )
+                ,EsZigbangDto.class
+        );
 
         for (var hit: search.hits().hits()) {
             EsZigbangDto res = hit.source();
